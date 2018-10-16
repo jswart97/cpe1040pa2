3468b9f2c2ca26946cbab7971c947590c68bc75f
jonsw <jswart1@msudenver.edu>
2018-10-15 22:25:14 -0600

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,1 +1,10 @@
-print(hi )
+def cap_join(string1):
+
+    names=[]
+    for index1 in string1:
+        names.append(index1[0].upper() + index1[1:])
+    return ' '.join(names)
+
+string2 = ("calvin", "and", "hobbes", "are", "the", "first", "spacemen", "on","mars")
+
+print(cap_join(string2))
